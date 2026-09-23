87d1590331bcb06d615439298b6b6bc64236cd60
MbazzaD <davidmbazza2@gmail.com>
2026-09-23 21:28:18 +0300

    fix: allow translated values to preserve array types

--- src/lib/language.ts
+++ src/lib/language.ts
@@ -1,5 +1,5 @@
 import {createContext,useContext} from 'react';
 
 export type AppLanguage='sw'|'en';
-export const LanguageContext=createContext<{language:AppLanguage;setLanguage:(v:AppLanguage)=>void;t:(sw:string,en:string)=>string}>({language:'sw',setLanguage:()=>{},t:(sw)=>sw});
+export const LanguageContext=createContext<{language:AppLanguage;setLanguage:(v:AppLanguage)=>void;t:<T>(sw:T,en:T)=>T}>({language:'sw',setLanguage:()=>{},t:<T>(sw:T)=>sw});
 export function useLanguage(){return useContext(LanguageContext)}
